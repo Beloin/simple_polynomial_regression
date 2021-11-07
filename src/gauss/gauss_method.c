1efb554ee1ef025f2ea774b50dd5cca307297864
Beloin Sena <beloin.rodrigues@lds.inf.br>
2021-11-07 18:43:37 -0300

    Gauss method WIP

--- src/gauss/gauss_method.c
+++ src/gauss/gauss_method.c
@@ -5,7 +5,12 @@
 #include <malloc.h>
 #include "gauss_method.h"
 
+void join_mx(int n, float a[n][n], float b[1][n], float inject_mx[n][n+1]);
+
 float *gauss_method(int arr_size, float a[arr_size][arr_size], float b[1][arr_size]){
+    float mx[arr_size][arr_size+1];
+    join_mx(arr_size, a, b, mx);
+    return gauss_method_unique_mx(arr_size, mx);
     float *arr = (float *) malloc(3 * sizeof (float));
     arr[0] = 0;
     arr[1] = 0;
@@ -15,7 +20,10 @@ float *gauss_method(int arr_size, float a[arr_size][arr_size], float b[1][arr_si
 
 void join_mx(int n, float a[n][n], float b[1][n], float inject_mx[n][n+1]){
     for (int i = 0; i < n; ++i) {
-
+        for (int ii = 0; ii < n+1; ++ii) {
+            if (ii == n) inject_mx[i][ii] = b[1][i];
+            else inject_mx[i][ii] = a[i][ii];
+        }
     }
 }
 
